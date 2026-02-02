02160b63d4113189e07d9920e0726c21eaa59a43
z216ero <moskinoEU@yandex.ru>
2026-02-03 02:29:56 +0300

    Add prod config

--- apps/mobile/src/config/env.ts
+++ apps/mobile/src/config/env.ts
@@ -1,1 +1,7 @@
-export const API_BASE_URL = 'http://10.0.2.2:5253';
+// env.ts
+const DEFAULT_API_URL = 'https://test-hn5h.onrender.com';
+
+// React Native CLI: process.env.* доступен только если ты его подставляешь на этапе сборки.
+// Поэтому делаем простой механизм через __DEV__.
+export const API_BASE_URL =
+  __DEV__ ? 'http://10.0.2.2:5253' : DEFAULT_API_URL;
